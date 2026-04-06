3cef9391001cb9c47c6a219124c348984c0435b8
AIO <Sebastian-Stepper@gmx.de>
2026-04-06 14:53:46 +0200

    chore: setup shadcn-vue

---
+++ app/components/ui/button/Button.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import type { PrimitiveProps } from 'reka-ui'
+import type { HTMLAttributes } from 'vue'
+import type { ButtonVariants } from '.'
+
+interface Props extends PrimitiveProps {
+  variant?: ButtonVariants['variant']
+  size?: ButtonVariants['size']
+  class?: HTMLAttributes['class']
+}
+
+const _props = withDefaults(defineProps<Props>(), {
+  as: 'button',
+})
+</script>
+
+<template>
+  <Primitive
+    data-slot="button"
+    :data-variant="variant"
+    :data-size="size"
+    :as="as"
+    :as-child="asChild"
+    :class="cn(buttonVariants({ variant, size }), props.class)"
+  >
+    <slot />
+  </Primitive>
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,29 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from '@tailwindcss/vite'
+
 export default defineNuxtConfig({
   compatibilityDate: '2026-04-06',
   devtools: { enabled: true },
+
   runtimeConfig: {
     oreApiUrl: 'http://localhost:8080',
     oreToken: '',
   },
+
+  css: ['~/assets/css/tailwind.css'],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
   nitro: {
     experimental: {
       websocket: true,
     },
+  },
+  modules: ['shadcn-nuxt'],
+  shadcn: {
+    prefix: '',
+    componentDir: '@/components/ui',
   },
 })
